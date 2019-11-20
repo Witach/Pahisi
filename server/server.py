7b0f21d26eecc1567d5dc083991060b1d7a50c87
Artur Wiśniewski <artwis1998@gmail.com>
2019-11-20 20:14:56 +0100

    Server GUI Buttons Events

--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ Created on Wed Nov 20 17:51:13 2019
 @author: Artur 
 """
 import sys
-from PyQt5.QtWidgets import QMainWindow,QAbstractButton, QApplication, QPushButton, QAction, QFileSystemModel,QTreeView, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel,QTextEdit
+from PyQt5.QtWidgets import QMainWindow,QAbstractButton, QApplication, QPushButton, QAction, QFileSystemModel,QTreeView, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel,QTextEdit,QSizePolicy
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 class MyMainWindow(QMainWindow):
@@ -47,30 +47,45 @@ class Widgets(QWidget):
         tree.resize(640, 480)
         return tree
 
-    
-        
+    def increment(self):
+        if self.numberOfPlayers < 4:
+            self.numberOfPlayers+=1
+        self.textBoxPlayers.setText('  ' + str(self.numberOfPlayers))
+    def dicriment(self):
+        if self.numberOfPlayers > 2:
+            self.numberOfPlayers-=1
+        self.textBoxPlayers.setText('  ' + str(self.numberOfPlayers))    
     
 
     def initUI(self):
+        self.numberOfPlayers = 2
         mainVerticalList = QVBoxLayout()
         labelport = QLabel('set port:')
         labelplayers = QLabel('set players:')
         
         textBoxPort = QLineEdit(self)
+        self.port = 6669
+        textBoxPort.setText(str(self.port))
         
         
         
-        textBoxPlayers = QTextEdit(self)
-        textBoxPlayers.setReadOnly(True)
-        textBoxPlayers.setMinimumHeight(10)
-        textBoxPlayers.setMaximumHeight(25)
-        textBoxPlayers.setMaximumWidth(30)
+        self.textBoxPlayers = QTextEdit(self)
+        self.textBoxPlayers.setReadOnly(True)
+        self.textBoxPlayers.setMinimumHeight(10)
+        self.textBoxPlayers.setMaximumHeight(25)
+        self.textBoxPlayers.setMaximumWidth(30)
+        
+        self.textBoxPlayers.setText('  ' + str(self.numberOfPlayers))
+        
         
         textBoxLogs = QTextEdit(self)
         textBoxLogs.setReadOnly(True)
         
         buttonDicriment = QPushButton('◄')
         buttonIncriment = QPushButton('►')
+        buttonIncriment.clicked.connect(self.increment)
+        buttonDicriment.clicked.connect(self.dicriment)
+        
         buttonDicriment.setMaximumWidth(50)
         buttonDicriment.setMinimumWidth(25)
         buttonIncriment.setMaximumWidth(50)
@@ -92,7 +107,7 @@ class Widgets(QWidget):
         playerHBox = QHBoxLayout()
         playerHBox.setAlignment(Qt.AlignLeft)
         playerHBox.addWidget(buttonDicriment)
-        playerHBox.addWidget(textBoxPlayers)
+        playerHBox.addWidget(self.textBoxPlayers)
         playerHBox.addWidget(buttonIncriment)
         
         interactVBox.addWidget(textBoxPort)
