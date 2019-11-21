da08940ce9e18ab30d67e6297092873c48437a95
Artur Wiśniewski <artwis1998@gmail.com>
2019-11-21 19:01:58 +0100

    Client GUI

---
+++ client/clientGUI.py
@@ -0,0 +1,34 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from .widgets import Widgets
+import math
+
+class MyMainWindow(QMainWindow):
+
+    title = 'Mensch ärgere Dich nicht'
+    left = 200
+    top = 40
+    width = 350
+    height = 500
+    screenHeight = 1080
+    screenWidth = 1920
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowIcon(QIcon('client/resources/dice.png'))
+        self.widgets = Widgets()
+        self.setCentralWidget(self.widgets)
+        self.initUIMainWindow()
+
+    def initUIMainWindow(self):
+        self.setWindowTitle(self.title)
+
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.move(
+            math.floor(self.screenWidth / 2 - self.height),
+            math.floor(self.screenHeight / 2 -400)
+        )
+
---
+++ client/widgets.py
@@ -0,0 +1,134 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from datetime import datetime
+
+class Widgets(QWidget):
+    numberOfPlayers = 2
+    port = 6669
+    startedPivot = False
+    adress = '127.0.0.1'
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+     
+        
+        textVBox = self.createTextVBox('set IP:', 'set port:')
+
+        splitter = self.createInteractBox(textVBox)
+        #movie =  self.createLabelGif('client/resources/pachisi.gif',110)
+        mainVerticalList = self.createMainVerticaLList(splitter,'client/resources/dice.png')
+
+        self.setLayout(mainVerticalList)
+        
+    def createLabelGif(self,filename,speed):
+        movie = self.loadFileIntoMovie(filename)
+        movieScreen = self.makeLabelfitTheGif()
+        self.addTheMovieOBjectToTheLabel(movie,speed,movieScreen)
+        return movieScreen
+        
+    def loadFileIntoMovie(self,filename):
+        movie = QMovie(filename, QByteArray(), self)
+
+        movie.setScaledSize(QSize().scaled(400,400,Qt.KeepAspectRatio))
+        return movie
+        
+    def makeLabelfitTheGif(self):
+        movie_screen = QLabel()
+        movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        movie_screen.setAlignment(Qt.AlignCenter)
+        
+        movie_screen.setStyleSheet("QLabel {padding: 0px 0px 0px 0px;}")
+        return movie_screen
+        
+    def addTheMovieOBjectToTheLabel(self, movie,speed,movie_screen):
+        movie.setCacheMode(QMovie.CacheAll)
+        movie.setSpeed(speed)
+        movie_screen.setMovie(movie)
+        movie.start()
+    
+        
+
+    def createMainVerticaLList(self, splitter,path):
+        
+        mainVerticalList = QVBoxLayout()
+        mainVerticalList.setContentsMargins(25,40,25,40)
+        mainVerticalList.setSpacing(7)
+        
+        mainVerticalList.addWidget(self.setImage(path))
+        mainVerticalList.addLayout(splitter)
+        mainVerticalList.addWidget(self.createStart(self.startGame,'Start'))
+        #mainVerticalList.addStretch(10)
+        return mainVerticalList
+    def setImage(self,path):
+        label = QLabel(self)
+        Image = QImage(path)
+        
+        label.setPixmap(QPixmap.fromImage(Image.scaled(300,300)))
+        label.setScaledContents(False)
+        
+        label.setAlignment(Qt.AlignCenter)
+        label.setStyleSheet("QLabel {padding: 30px 30px 30px 30px;}")
+        return label
+
+    def createInteractBox(self, textVBox):
+        interactVBox = QVBoxLayout()
+        splitter = QHBoxLayout()
+        splitter.addLayout(textVBox)
+        splitter.addLayout(interactVBox)
+        self.textBoxPort = QLineEdit(self)
+        self.textBoxPort.setStyleSheet("QLineEdit {font-size: 15px;font-family: Arial;padding: 5px 5px 5px 5px;}")
+        self.textBoxAdress = QLineEdit(self)
+        self.textBoxAdress.setStyleSheet("QLineEdit {font-size: 15px;font-family: Arial;padding: 5px 5px 5px 5px;}")
+        self.textBoxPort.setText(str(self.port))
+        self.textBoxAdress.setText(str(self.adress))
+        
+        interactVBox.addWidget(self.textBoxAdress)
+        interactVBox.addWidget(self.textBoxPort)
+        
+        return splitter
+
+    def createPlayerHbox(self, buttonDicriment, buttonIncriment):
+        playerHBox = QHBoxLayout()
+        playerHBox.setAlignment(Qt.AlignLeft)
+        for widget in [buttonDicriment, self.textBoxPlayers, buttonIncriment]:
+            playerHBox.addWidget(widget)
+        return playerHBox
+
+    def createTextVBox(self, portText, playerText):
+        textVBox = QVBoxLayout()
+        l1 = QLabel(portText)
+        l2 = QLabel(playerText)
+        l1.setStyleSheet("QLabel {font-size: 15px;font-family: Arial;padding: 5px 5px 5px 5px;}")
+        l2.setStyleSheet("QLabel {font-size: 15px;font-family: Arial;padding: 5px 5px 5px 5px;}")
+        textVBox.addWidget(l1)
+        textVBox.addWidget(l2)
+        return textVBox
+
+    def createButton(self, fun, icon, maxWidth=50, minWidth=25):
+        button = QPushButton(icon)
+        button.clicked.connect(fun)
+        button.setMaximumWidth(maxWidth)
+        button.setMinimumWidth(minWidth)
+        return button
+    def createStart(self, method, name):
+        button = QPushButton(name)
+       
+        #button.setContentsMargins(10,30,30,30)
+        button.setStyleSheet("QPushButton {font-size: 25px;font-family: Arial;padding: 10px 5px 10px 5px;}")
+        button.clicked.connect(method)
+        return button
+    def startGame(self):
+        print('start!')
+   
+
+    
+
+        
+        
+
+  
