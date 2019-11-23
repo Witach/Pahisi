2274d296414fc3c4220b68efe8611b8f8abf1c41
Artur Wiśniewski <artwis1998@gmail.com>
2019-11-23 21:46:12 +0100

    client game Launch from gamePanel!

--- client/game/gamePanel.py
+++ client/game/gamePanel.py
@@ -3,21 +3,35 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from win32api import GetSystemMetrics
+import time
+import threading
 class GamePanel(QWidget):
     port = 6669
+    connected = True
     ipAdress = '127.0.0.1'
     
-    def __init__(self,ipAdress = '127.0.0.1',port = 6669):
+    def __init__(self,app,ipAdress = '127.0.0.1',port = 6669):
        super().__init__()
+       app.setStyleSheet('QWidget{background-color:black;}')
        self.port = port
        self.ipAdress = ipAdress
-       self.init()
+       ##GIF WAITING staff
+       self.initGifWaiting()
+       ##WAITING FOR CONNECTION THREAD
+       x = threading.Thread(target=self.waitingForConnection, args=())
+       x.start()
+       
        self.show()
+    def initGifWaiting(self):
+        self.initScreenSize()
+        self.movie =  self.createLabelGif('resources/waiting.gif',75)
+        self.oLayout = QVBoxLayout()
+        self.oLayout.addWidget(self.movie)
+        self.setLayout(self.oLayout)
     def init(self):
-        
        screenWidth = self.initScreenSize()
        self.initBackground(screenWidth)
-       
+    #Screen Settings   
     def initScreenSize(self):
        screenWidth = (int)(GetSystemMetrics(1) -  0.08*GetSystemMetrics(1))
        self.move((int)(GetSystemMetrics(1)/2),0)
@@ -31,8 +45,47 @@ class GamePanel(QWidget):
        palette = QPalette()
        palette.setBrush(QPalette.Background, QBrush(sImage))                        
        self.setPalette(palette)
-   
+       
+    ## GIF STAFF   
+    def createLabelGif(self,filename,speed):
+        movie = self.loadFileIntoMovie(filename)
+        movieScreen = self.makeLabelfitTheGif()
+        self.addTheMovieOBjectToTheLabel(movie,speed,movieScreen)
+        return movieScreen
         
+    def loadFileIntoMovie(self,filename):
+        movie = QMovie(filename, QByteArray(), self)
+        movie.setScaledSize(QSize().scaled(GetSystemMetrics(1) -  0.08*GetSystemMetrics(1),GetSystemMetrics(1) -  0.08*GetSystemMetrics(1),Qt.KeepAspectRatio))
+        return movie
+        
+    def makeLabelfitTheGif(self):
+        movie_screen = QLabel()
+        movie_screen.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        movie_screen.setAlignment(Qt.AlignCenter)
+        return movie_screen   
+    
+    def addTheMovieOBjectToTheLabel(self, movie,speed,movie_screen):
+        movie.setCacheMode(QMovie.CacheAll)
+        movie.setSpeed(speed)
+        movie_screen.setMovie(movie)
+        movie.start()  
+     ## GIF STAFF 
+     ## Waiting for connection
+    def waitingForConnection(self):
+        #wait for connection in loop
+        #need connection checked
+        
+        time.sleep(2)
+        
+        if self.connected:
+            #if connected then delete waiting gif
+            self.oLayout.itemAt(0).widget().setParent(None)
+            self.init()
+             
+   
+
+          
+    
 
       
 
@@ -41,6 +94,11 @@ class GamePanel(QWidget):
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet('QMainWindow{background-color:orange;border: 1px solid black;}')
-    oMainwindow = GamePanel()
+    app.setStyleSheet('QWidget{background-color:black}')
+    oMainwindow = GamePanel(app)
     sys.exit(app.exec_())
+    
+    
+
+   
+
